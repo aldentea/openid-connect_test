5f17387da6c83735b428a70d5273b505754d41ff
aldentea <aldentea-dev@telepathy.aldentea.0g0.jp>
2018-03-05 15:37:03 +0900

    Implement AuthController.

--- controller/init.rb
+++ controller/init.rb
@@ -1,12 +1,33 @@
-# Define a subclass of Ramaze::Controller holding your defaults for all controllers. Note 
-# that these changes can be overwritten in sub controllers by simply calling the method 
+# Define a subclass of Ramaze::Controller holding your defaults for all controllers. Note
+# that these changes can be overwritten in sub controllers by simply calling the method
 # but with a different value.
 
 class Controller < Ramaze::Controller
   layout :default
   helper :xhtml
   engine :etanni
+
+
+  # Helper methods
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def authenticate
+    return if logged_in?
+    #redirect_to root_path
+  end
+
+
 end
+
+
 
 # Here you can require all your other controllers. Note that if you have multiple
 # controllers you might want to do something like the following:
@@ -15,4 +36,6 @@ end
 #    require(controller)
 #  end
 #
-require __DIR__('main')
+require_relative 'main'
+require_relative 'auth'
+
